98dce833fdb3f3545153142090df01436663c350
colin.a.horner <HORNER_COLIN@token.co.jp>
2020-03-10 12:58:58 +0900

    [Mod] Remove foreign keys for now until I can get the basic structure working

--- lib/spy_alley_api_mysql/relations/actions.rb
+++ lib/spy_alley_api_mysql/relations/actions.rb
@@ -4,7 +4,7 @@ require 'rom-sql'
 module SpyAlleyApiMysql
   module Relations
     class Actions < ROM::Relation[:sql]
-      schema do
+      schema(:actions) do
         attribute :id, Dry::Types['strict.int']
         attribute :last_action_id, Dry::Types['strict.int']
         attribute :next_player_id, Dry::Types['strict.int']
@@ -13,10 +13,10 @@ module SpyAlleyApiMysql
 
         primary_key :id
 
-        associations do
-          has_many :deck_starts, combine_key: :start_action_id
-          has_many :deck_ends, combine_key: :end_action_id
-        end
+        #associations do
+        #  has_many :deck_starts, combine_key: :start_action_id
+        #  has_many :deck_ends, combine_key: :end_action_id
+        #end
       end
     end
   end
--- lib/spy_alley_api_mysql/relations/decks.rb
+++ lib/spy_alley_api_mysql/relations/decks.rb
@@ -4,7 +4,7 @@ require 'rom-sql'
 module SpyAlleyApiMysql
   module Relations
     class Decks < ROM::Relation[:sql]
-      schema do
+      schema(:decks) do
         attribute :id, Dry::Types['strict.int']
         attribute :game_id, Dry::Types['strict.int']
         attribute :contents, Dry::Types['strict.string']
@@ -13,10 +13,10 @@ module SpyAlleyApiMysql
 
         primary_key :id
 
-        associations do
-          belongs_to :start_action, combine_key: :start_action_id
-          belongs_to :end_action, combine_key: :end_action_id
-        end
+        #associations do
+        #  belongs_to :start_action, combine_key: :start_action_id
+        #  belongs_to :end_action, combine_key: :end_action_id
+        #end
       end
     end
   end
